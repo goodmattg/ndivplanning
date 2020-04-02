fixed visdom error plotting. Switch forward training average loss to every epoch. Switch to StepLR optimizer in three stages

train_forward_model.py
@@ -30,11 +30,12 @@ from tqdm import tqdm
 from torch.optim.lr_scheduler import StepLR
 from utils.trajectory_loader import PushDataset
 from models.forward_encoder import Decoder, Encoder
+
 # Configurations and Hyperparameters
 port_num = 8082
 gpu_id = 1
 lr_rate = 2e-4
-num_epochs = 50
+num_epochs = 30
 num_sample = 6
 noise_dim = 2
 report_feq = 10
@@ -44,7 +45,7 @@ np.random.seed(1)
 
 # Dataloader
 # gpu_id = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-dataset = PushDataset("data")
+dataset = PushDataset("data", seq_length=12)
 loader = data.DataLoader(dataset, batch_size=8, shuffle=True)
 
 # Models
@@ -54,7 +55,7 @@ decoder.weight_init(mean=0.0, std=0.02)
 encoder.weight_init(mean=0.0, std=0.02)
 
 # Initialize Loss
-l1, mse, bce = nn.L1Loss(), nn.MSELoss(), nn.BCELoss()
+mse = nn.MSELoss()
 
 # Initialize Optimizer
 optimizer = optim.Adam(
@@ -63,48 +64,55 @@ optimizer = optim.Adam(
     betas=(0.5, 0.999),
 )
 
+torch.optim.lr_scheduler.StepLR(optimizer, num_epochs // 3, gamma=0.1)
+
 step = 0
 min_pred_error = np.inf
 for epoch in range(num_epochs):
+    loss_np_sum = 0
+
     for i, inputs in enumerate(loader):
         images, _, actions = inputs
-        loss_np_sum = 0
-        for image_num in range(actions.shape[1] -1):
-            state_cur = images[:,image_num]
-            state_fut = images[:,image_num+1]
-            
-            code,feats = encoder(state_cur)
-            state_action_concate = torch.cat([code,actions[:,image_num]],dim=1)
-            state_action_concate = state_action_concate.unsqueeze(2).unsqueeze(3)
-            state_fut_hat = decoder(state_action_concate,feats)
 
-            loss = mse(state_fut_hat,state_fut)
+        for image_num in range(dataset.seq_length - 1):
+            state_cur = images[:, image_num]
+            state_fut = images[:, image_num + 1]
+
+            code, feats = encoder(state_cur)
+            state_action_concate = torch.cat([code, actions[:, image_num]], dim=1)
+            state_action_concate = state_action_concate.unsqueeze(2).unsqueeze(3)
+            state_fut_hat = decoder(state_action_concate, feats)
+
+            loss = mse(state_fut_hat, state_fut)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            step +=1
+            step += 1
             loss_np = loss.cpu().data.numpy()
             loss_np_sum += loss_np
 
-        avg_loss = loss_np_sum/(actions.shape[1]-1)
-        print(epoch,step,"reconstruction loss per batch:", avg_loss)
-        
-        err_dict = {"loss_np:",avg_loss}
-        display.plot_error(error_dict)
-
         if step % report_feq == 0:
-            state_cur_vis = [denorm(state_cur[0]).detach().cpu().numpy().astype(np.uint8)]
-            state_fut_vis = [denorm(state_fut[0]).detach().cpu().numpy().astype(np.uint8)]
-            state_fut_hat_vis = [denorm(state_fut_hat[0]).detach().cpu().numpy().astype(np.uint8)]
+            state_cur_vis = [
+                denorm(state_cur[0]).detach().cpu().numpy().astype(np.uint8)
+            ]
+            state_fut_vis = [
+                denorm(state_fut[0]).detach().cpu().numpy().astype(np.uint8)
+            ]
+            state_fut_hat_vis = [
+                denorm(state_fut_hat[0]).detach().cpu().numpy().astype(np.uint8)
+            ]
             display.img_result(state_cur_vis, win=1, caption="state_cur_vis")
             display.img_result(state_fut_vis, win=2, caption="state_fut_vis")
             display.img_result(state_fut_hat_vis, win=3, caption="state_fut_hat_vis")
 
-        if epoch%10 == 9:
-            if not os.path.exists('models'):os.makedirs('models')
-            torch.save(encoder, 'models/encoder_'+str(epoch)+'.pt')
-            torch.save(decoder, 'models/decoder_'+str(epoch)+'.pt')      
+    # Log average epoch loss
+    avg_loss = loss_np_sum / ((dataset.seq_length - 1) * len(loader))
+    display.plot("loss", "train", "Forward Model Loss", epoch, avg_loss)
+    print(epoch, step, "reconstruction loss per epoch:", avg_loss)
 
-
-
+    if epoch % (num_epochs // 3) == (num_epochs // 3) - 1:
+        if not os.path.exists("models"):
+            os.makedirs("models")
+        torch.save(encoder, "models/forward_encoder_" + str(epoch) + ".pt")
+        torch.save(decoder, "models/forward_decoder_" + str(epoch) + ".pt")